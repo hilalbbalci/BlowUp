269251457503e5f500165e90d12fe753049f3823
hilalbbalci <drhilalbiten@gmail.com>
2021-08-06 11:06:59 -0700

    done with that branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    validates :email, :username, :session_token, :password_digest, presence: true
-    validates :email, :username, :session_token, uniqueness: true
+    validates :username, :session_token, :password_digest, presence: true
+    validates :username, :session_token, uniqueness: true
     validates :password, length: { minimum: 6, allow_nil: true}
     after_initialize :ensure_session_token
     
