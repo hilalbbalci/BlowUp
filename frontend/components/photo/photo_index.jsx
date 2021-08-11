ddafb2ae017b5f054c702ce91e44dd9262d9eda9
hilalbbalci <drhilalbiten@gmail.com>
2021-08-11 15:49:13 -0700

    do css

--- frontend/components/photo/photo_index.jsx
+++ frontend/components/photo/photo_index.jsx
@@ -19,51 +19,122 @@ class PhotoIndex extends React.Component {
                     <h3>Welcome to BlowUp</h3>
                     <p>Follow photographers to get started</p>
                 </div>
-                
 
-                <div className="photos-slices-container">
-
-                    <ul className="photo-item-container">
-                        {photos.slice(0, 3).map((photo, idx) => (
-                            <li className="photo-item" key={idx}>
-                                <Link to={`/photos/${photo.id}`} >
-                                    <img className="photo-img" src={photo.post} />
-                                </Link>
-                            </li>
-                        ))}
-                        </ul>
-                    <ul className="photo-item-container">
-                        {photos.slice(3, 6).map((photo, idx) => (
+                <div className="featured-photos">
+                    <h2>Featured photographers</h2>
+                    <p>Follow to explore new works</p>
+                    <div className="photos-slices-container">
+                        <ul className="photo-item-container">
+                            {photos.slice(0, 3).map((photo, idx) => (
                                 <li className="photo-item" key={idx}>
                                     <Link to={`/photos/${photo.id}`} >
                                         <img className="photo-img" src={photo.post} />
                                     </Link>
                                 </li>
-                        ))};
-                    </ul>
-                    <ul className="photo-item-container">
-                        {photos.slice(6, 9).map((photo, idx) => (
-                            <li className="photo-item" key={idx}>
-                                <Link to={`/photos/${photo.id}`} >
-                                    <img className="photo-img" src={photo.post} />
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
+                            ))}
+                        </ul>
+                        <ul className="photo-item-container">
+                            {photos.slice(3, 6).map((photo, idx) => (
+                                <li className="photo-item" key={idx}>
+                                    <Link to={`/photos/${photo.id}`} >
+                                        <img className="photo-img" src={photo.post} />
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                        <ul className="photo-item-container">
+                            {photos.slice(6, 9).map((photo, idx) => (
+                                <li className="photo-item" key={idx}>
+                                    <Link to={`/photos/${photo.id}`} >
+                                        <img className="photo-img" src={photo.post} />
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
+                    
                 <div className="photos-all" >
-                    {photos.map((photo, idx) => (
-                        <div key={idx}>
-                            <Link to={`/photos/${photo.id}`} >
-                                <img className="photo-img" src={photo.post} />
-                            </Link>
+                    <div className="one-section">
+                        <h2>Editors' Choice</h2>
+                        <p>Photos selected by our Editors</p>
+                        <div className="column">
+                            <div className="row">    
+                                {photos.slice(9, 11).map((photo, idx) => (
+                                <div key={idx}>
+                                    <Link to={`/photos/${photo.id}`} >
+                                        <img className="photo-img" src={photo.post} />
+                                    </Link>
+                                </div>
+                                ))}
+                            </div>
+                            <div className="row">
+                            {photos.slice(11, 13).map((photo, idx) => (
+                                <div key={idx}>
+                                    <Link to={`/photos/${photo.id}`} >
+                                        <img className="photo-img" src={photo.post} />
+                                    </Link>
+                                </div>
+                                ))}
+                            </div>
                         </div>
-                    ))}
+                    </div>
+                    <div className="one-section">
+                        <h2>Featured Galleries</h2>
+                        <p>Best Galeries o BlowUp</p>
+                        <div className="column">
+                            <div className="row">
+                                {photos.slice(17, 19).map((photo, idx) => (
+                                    <div key={idx}>
+                                        <Link to={`/photos/${photo.id}`} >
+                                            <img className="photo-img" src={photo.post} />
+                                        </Link>
+                                    </div>
+                                ))}
+                            </div>
+                            <div className="row">
+                                {photos.slice(19, 21).map((photo, idx) => (
+                                    <div key={idx}>
+                                        <Link to={`/photos/${photo.id}`} >
+                                            <img className="photo-img" src={photo.post} />
+                                        </Link>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="one-section">
+                        <h2>Quests</h2>
+                        <p>Photo contests to challange you</p>
+                        <div className="column">
+                            <div className="row">
+                                {photos.slice(13, 15).map((photo, idx) => (
+                                    <div key={idx}>
+                                        <Link to={`/photos/${photo.id}`} >
+                                            <img className="photo-img" src={photo.post} />
+                                        </Link>
+                                    </div>
+                                ))}
+                            </div>
+                            <div className="row">
+                                {photos.slice(15, 17).map((photo, idx) => (
+                                    <div key={idx}>
+                                        <Link to={`/photos/${photo.id}`} >
+                                            <img className="photo-img" src={photo.post} />
+                                        </Link>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="last-div">
+
                 </div>
             </div>
         )
     }
 
-}
+};
 
 export default PhotoIndex;
